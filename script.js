2d1c21e0a7720fe2190320906e5dccc7503c1ea1
Hack Reactor Student <student@hackreactor.com>
2017-06-21 16:55:38 -0500

    finished the first number

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 $(document).ready(function() {
 
   /* Your Javascript Here */
-function answer() {
-num1 fun1 num2
-}
+// function answer() {
+// num1 fun1 num2
+// }
+
+  var current = $("#name").text()
+  $(".ma").click(function (e) {
+      $("#name").text(current +e.target.innerHTML);
+      current = $("#name").text()
+      console.log(e.target.innerHTML)
+  })
 });
