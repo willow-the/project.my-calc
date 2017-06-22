372b9d7012db0ac54eb466647a5e449c6da23c0c
Hack Reactor Student <student@hackreactor.com>
2017-06-22 16:59:09 -0500

    got it functioning off and on

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
 $(document).ready(function() {
-
-  /* Your Javascript Here */
-// function answer() {
-// num1 fun1 num2
-// }
-
-  var num1 = $("#name").text()
-  $(".ma").click(function (e) {
-      $("#name").text(num1 +e.target.innerHTML);
-      num1 = $("#name").text()
+  var numr = []
+  var d = ("1")
+  var opp = $("textarea").val()
+  // var numr = $("textarea").val()
+  var numb = $("textarea").val()
+  if(d==="1"){
+    $(".ma").click(function (e) {
+        numr.push($("textarea").val())
+        var li = numr + e.target.innerHTML
+        $("textarea").val(li)
+    })
+  } else {
+        $(".ma").click(function (e) {
+          numb = $("textarea").val()
+          var lio = numb + e.target.innerHTML
+          $("textarea").val(lio)
+        })
+  }
+  $(".a").click(function(a){
+      console.log(numr)
+      numr.push($("textarea").val())
+      $("textarea").val("")
+      opp = a.target.innerHTML
+      console.log(opp)
+      d = ("2")
+  })
+  $("#a").click(function () {
+      numb = $("textarea").val()
+      var ans = (numr +opp+ numb)
+      console.log(ans)
+      console.log( eval(ans))
+      $("p").val(eval(ans))
   })
 });
