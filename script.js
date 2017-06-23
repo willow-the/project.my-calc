cddfd70ecbecfff26c6673836ef207fb90163d6a
Hack Reactor Student <student@hackreactor.com>
2017-06-23 14:45:21 -0500

    got it working and styled it

--- script.js
+++ script.js
@@ -1,48 +1,62 @@
 $(document).ready(function() {
-  var numr = []
+// has it run the JavaScript.
+
   var d = ("1")
+// sets up a counter for which number variable to work on 
   var opp = $("textarea").val()
-  // var numr = $("textarea").val()
+// operation variable
+  var numr = $("textarea").val()
+// first number varriable
   var numb = $("textarea").val()
-  // if(d==="1"){
-  //   $(".ma").click(function (e) {
-  //       // numr.push($("textarea").val())
-  //       var li = numr + e.target.innerHTML
-  //       $("textarea").val(li)
-  //   })
-  // } else {
-  //       $(".ma").click(function (e) {
-  //         numb = $("textarea").val()
-  //         var lio = numb + e.target.innerHTML
-  //         $("textarea").val(lio)
-  //       })
-  // }
+// second number varriable
+// sets up variables
+
   $(".a").click(function(a){
       console.log(numr)
-      numr.push($("textarea").val())
+      numr=$("textarea").val()
       $("textarea").val("")
       opp = a.target.innerHTML
       console.log(opp)
       d = ("2")
+// makes operation buttons work.
   })
+
   $("#a").click(function () {
       numb = $("textarea").val()
       var ans = (numr +opp+ numb)
       console.log(ans)
-      console.log( eval(ans))
-      $("p").val(eval(ans))
+      console.log(eval(ans))
+      $("p").text(eval(ans))
+// makes equals button work and displays answer.
   })
 
+  $("#v").click(function () {
+      $("textarea").val("")
+      numb = $("textarea").val()
+      numr = $("textarea").val()
+      numbl = $("textarea").val()
+      d=("1")
+      ans = $("textarea").val()
+// makes clear "nuke" button work.
+  })
 
     $(".ma").click(function (e) {
+// makes number buttons work.
+
       if(d==="1"){
-        // numr.push($("textarea").val())
+// changes which variable is being saved.
+
+        numr= $("textarea").val()
         var li = numr + e.target.innerHTML
         $("textarea").val(li)
+// first number
+
       } else {
+
           numb = $("textarea").val()
           var lio = numb + e.target.innerHTML
           $("textarea").val(lio)
+// second number
       }
     })
 });
